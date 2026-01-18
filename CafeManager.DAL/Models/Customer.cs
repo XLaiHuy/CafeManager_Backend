df5bb46d6ac089883a93c585d9fe47b41d4b9cd3
Phan Cao Huy <youremail@gmail.com>
2026-01-18 12:06:51 +0700

    h

--- CafeManager.DAL/Models/Customer.cs
+++ CafeManager.DAL/Models/Customer.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CafeManager.DAL.Models;
 
 public partial class Customer
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 
     public string Name { get; set; } = null!;
